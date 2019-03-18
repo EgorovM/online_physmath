14b1a8b740a8df71432a1f7c1a4638de1a9d28dc
EgorovM <egorov_michil@mail.ru>
2019-03-18 19:37:42 +0900

    debug false

--- online_physmath/settings.py
+++ online_physmath/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_^#9e4ir3f5yev_v=b6%^h2m2t2q5)e#e8iqvm&dkbjo_gdn5t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
